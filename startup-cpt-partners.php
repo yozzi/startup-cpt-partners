d48265fa7e7cf0cacdf056ec866abe9dd27bbb9d
yozz <yozz.online@gmail.com>
2015-11-18 14:49:08 -0500

    Disabled Github Updater

--- startup-cpt-partners.php
+++ startup-cpt-partners.php
@@ -29,7 +29,7 @@ function startup_reloaded_partners_updater() {
 	}
 }
 
-add_action( 'init', 'startup_reloaded_partners_updater' );
+//add_action( 'init', 'startup_reloaded_partners_updater' );
 
 //CPT
 function startup_reloaded_partners() {

1ee97ac498d60c08984fbaea44c42fff7deca3cb
yozz <yozz.online@gmail.com>
2016-01-13 16:38:26 -0500

    Format

--- startup-cpt-partners.php
+++ startup-cpt-partners.php
@@ -180,7 +180,7 @@ if ( !function_exists( 'shortcode_ui_detection' ) ) {
         }
     }
 
-add_action( 'init', 'shortcode_ui_detection' );
+    add_action( 'init', 'shortcode_ui_detection' );
 }
 
 function startup_cpt_partners_shortcode_ui() {
